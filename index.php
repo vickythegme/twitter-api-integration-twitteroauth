58fb7f64562e0d5aa52597906d6bf5df93ccba35
Vignesh <vickythegme@gmail.com>
2014-08-14 05:53:55 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>
+		Twitter API Integration With Abraham's Twitteroauth Example
+	</title>
+</head>
+<body>
+	<h1>Twitter API Integration With Abraham's Twitteroauth Example</h1>
+	<p>
+		<a href="redirect.php">Sign In With Twitter</a>
+	</p>
+</body>
+</html>
